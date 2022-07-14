12eb6124b40c2ed4ddfe31f46b8daf745981a299
rowenyou <rowen.you@gmail.com>
2022-07-14 12:56:52 +0900

    깃 변경

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동 맛집"]);
+  let [따봉갯수, 따봉갯수변경] = useState(0);
+  let date = "2021. 12. 13";
+  var data = "안녕하세요";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 blog</div>
+        <div>{data}</div>
+      </div>
+      <div className="list">
+        <h3>
+          {글제목[0]} <span>?</span> {따봉갯수}{" "}
+        </h3>
+        <p>{date}</p>
+      </div>
+      <div className="list">
+        <h3>
+          {글제목[1]} <span>?</span> {따봉갯수}{" "}
+        </h3>
+        <p>{date}</p>
+      </div>
     </div>
-    
   );
 }
 
